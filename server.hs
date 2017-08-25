1586689e04fdf72e4f448d84552bb2bb6cfa3572
Alan Rempel <alan.rempel@gmail.com>
2017-08-25 16:01:53 -0500

    Add fileserver

---
+++ server.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE ViewPatterns, OverloadedStrings #-}
+module Main where
+
+import Data.Monoid ( (<>) )
+import Network.Wai
+import Network.Wai.Application.Static
+import Network.Wai.Handler.Warp
+import WaiAppStatic.Types
+
+import System.Environment
+
+main :: IO ()
+main = do
+    args <- getArgs
+    let (port, htmlDir) =
+            case args of
+                [ port, htmlDir ] -> ( read port, htmlDir )
+                [ htmlDir ] -> ( 8000, htmlDir )
+                [] -> ( 8000, "serve" )
+                _ -> error $ "Dodgy args: " <> show args
+    run port $ app htmlDir
+
+app :: FilePath -> Application
+app htmlDir =
+    let defaults = defaultWebAppSettings htmlDir
+     in staticApp $ defaults { ssLookupFile = ssLookupFile defaults . route }
+  where
+    route (map fromPiece -> pieces) = map unsafeToPiece $
+        case pieces of
+            [] -> [ "index.html" ]
+            [ "archive" ] -> [ "archive.html" ]
+            [ "posts" , post ] -> [ "posts", post <> ".html" ]
+            _ -> pieces
+
