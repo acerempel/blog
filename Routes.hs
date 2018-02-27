5c9df4d41bf27da3f4e1a7b2cd66966fc35c349e
Parson Yorick <alan.rempel+parsonyorick@gmail.com>
2018-02-27 12:20:38 -0400

    Simplify Routes.hs

--- Routes.hs
+++ Routes.hs
@@ -9,45 +9,28 @@ import qualified Post
 import Site
 
 
-data Route
-   = Home
-   | Archive
-   | Post Text Bool
-   | Stylesheet FilePath
-
-url :: Route -> SiteM URI
-url route _config =
+url :: String -> SiteM URI
+url finalPiece _config =
    fromJust (parseRelativeReference ("/" <> finalPiece))
- where
-   finalPiece =
-      case route of
-         Home      ->
-            ""
-         Archive   ->
-            "archive.html"
-         -- TODO: Use System.FilePath operators?
-         -- TODO: Should posts/ really be hardcoded here? The thing is it
-         -- has to be the same as postsDir in Shakefile.hs.
-         Post slug False ->
-            "posts/" <> Text.unpack slug <> ".html"
-         Post slug True ->
-            "drafts/" <> Text.unpack slug <> ".html"
-         -- These already have stylesDir prepended.
-         Stylesheet filename ->
-            filename
 
 urlForPost :: Post -> SiteM URI
 urlForPost Post.Post{ slug, isDraft } =
-   url (Post slug isDraft)
+   url $ dir <> Text.unpack slug <> ".html"
+ where
+   -- TODO: Use System.FilePath operators?
+   -- TODO: Should posts/ really be hardcoded here? The thing is it
+   -- has to be the same as postsDir in Shakefile.hs.
+   dir = if isDraft then "drafts/" else "posts/"
 
 urlForStylesheet :: FilePath -> SiteM URI
 urlForStylesheet ss =
-   url (Stylesheet ss)
+   -- These already have stylesDir prepended.
+   url ss
 
 homeUrl :: SiteM URI
 homeUrl =
-   url Home
+   url ""
 
 archiveUrl :: SiteM URI
 archiveUrl =
-   url Archive
+   url "archive.html"
