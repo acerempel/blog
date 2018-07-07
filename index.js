e3ad94f348cde9423547ae9bc4b06d57b79c9a3e
Parson Yorick <alan.rempel+parsonyorick@gmail.com>
2018-07-07 13:52:19 -0300

    Reformat; put onclick handler on div, not h2

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const state = { greeting: 0 }
 
 const greetings = ["Good evening!", "Good morning!", "Good afternoon!"]
 
-const actions = { next: state => ( { greeting: (state.greeting + 1) % 3 } ) }
+const actions = {
+  next: value => state => {
+    return { greeting: (state.greeting + 1) % 3 }
+  }
+}
 
 const view = (state, actions) => (
-  <div>
-    <h2 onclick={actions.next}> {greetings[state.greeting]}</h2>
+  <div onclick={actions.next}>
+    <h2>{greetings[state.greeting]}</h2>
   </div>
 )
 
