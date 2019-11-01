516275a708638f9f3200d2dc9772107abd07d544
Parson Yorick <alan.rempel+parsonyorick@gmail.com>
2019-11-01 09:52:04 -0300

    Store user's colour-scheme setting in local storage

--- site-src/scripts/colour-scheme.js
+++ site-src/scripts/colour-scheme.js
@@ -4,9 +4,18 @@ function setColourScheme(prefix, colourScheme, element) {
   var colourSchemeClassName = prefix + colourScheme;
   newClasses.push(colourSchemeClassName);
   element.setAttribute("class", newClasses.join(" "));
+  window.localStorage.setItem("colour-scheme", colourScheme);
 }
 
 function changeColourScheme(prefix, selector) {
   var element = document.querySelector(selector);
   setColourScheme(prefix, event.target.value, element);
 }
+
+// This script should have the 'defer' attribute set, so that the
+// 'DOMContentLoaded' event will not yet have fired when it is run.
+document.addEventListener("DOMContentLoaded", function(_event) {
+  var select = document.getElementById("colour-scheme-select");
+  var stored = window.localStorage.getItem("colour-scheme");
+  if (stored) { select.value = stored }
+});
